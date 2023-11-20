1fd541cb1f21c5970f88d6e4ca6add18dca1721d
Sachin R <32716246+sachindoddaguni@users.noreply.github.com>
2023-11-20 11:22:56 -0500

    Updating bash init script  to enable multiple python paths

--- configurations/project_init.sh
+++ configurations/project_init.sh
@@ -6,18 +6,26 @@ PROJECT_DIR=${SCRIPT_DIR%/*}
 
 RELATIVE_GITS_PATH="code/gits.py"
 
+# Check if python3 is available, otherwise use python
+if command -v python3 &> /dev/null; then
+    PYTHON_COMMAND="python3"
+else
+    echo "Warning: python3 is not available, falling back to python."
+    PYTHON_COMMAND="python"
+fi
+
 GITS_EXEC_PATH="${PROJECT_DIR}/${RELATIVE_GITS_PATH}"
 
 BASHRC=~/.bashrc
 if [ -f "$BASHRC" ]; then
     echo "$BASHRC exists, appending gits commandline tool alias"
-    echo "alias gits=\"python3 $GITS_EXEC_PATH\"" >> $BASHRC
+    echo "alias gits=\"$PYTHON_COMMAND $GITS_EXEC_PATH\"" >> $BASHRC
 else
     echo "$BASHRC does not exist, creating a new file and adding gits commandline tool alias"
-    echo "alias gits=\"python3 $GITS_EXEC_PATH\"" >> $BASHRC
+    echo "alias gits=\"$PYTHON_COMMAND $GITS_EXEC_PATH\"" >> $BASHRC
 fi
 
-echo "Initialising gits directory in user home directory"
+echo "Initializing gits directory in user home directory"
 
 GITS=~/.gits
 GITS_LOG=~/.gits/logs
